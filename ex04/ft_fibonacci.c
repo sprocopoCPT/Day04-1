7763aa72c3425a70bf1527dff5790516421947db
sprocopoCPT <67952406+sprocopoCPT@users.noreply.github.com>
2020-07-16 09:57:00 +0200

    Update ft_fibonacci.c

--- ex04/ft_fibonacci.c
+++ ex04/ft_fibonacci.c
@@ -5,14 +5,17 @@ int ft_fibonacci(int index)
     return -1;
   }
 
-  if (index == 0)
+  else if (index == 0)
   {
     return 0;
   }
 
-  if (index == 1)
+  else if (index == 1)
   {
     return 1;
   }
-else return (ft_fibonacci(index -1) + ft_fibonacci(index -2));
+else 
+{
+  return (ft_fibonacci(index -1) + ft_fibonacci(index -2));
+}
 }
